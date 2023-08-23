94831014b8a7ed034a3f989b29b2f248bad3a37e
mkskh <maksym.kharchenko@dci-student.org>
2023-08-23 15:47:01 +0200

    made first task

---
+++ task1.py
@@ -0,0 +1,17 @@
+num = int(input('Enter number: '))
+if (num % 2) == 0:
+    print(num, 'is even number') #made it a little more difficult
+else: 
+    print(num, 'is odd number') #made it a little more difficult
+    
+num = int(input('Enter number: '))
+if (num % 2) == 0:
+    print(num, 'is even number')
+else: 
+    print(num, 'is odd number')
+
+num = int(input('Enter number: '))
+if (num % 2) == 0:
+    print(num, 'is even number')
+else: 
+    print(num, 'is odd number')
