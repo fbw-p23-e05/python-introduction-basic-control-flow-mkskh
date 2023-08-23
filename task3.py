b8e67e3c0959181d8ffa9421ab5655bf0b461f64
mkskh <maksym.kharchenko@dci-student.org>
2023-08-23 15:59:57 +0200

    made third task

---
+++ task3.py
@@ -0,0 +1,7 @@
+num1 = int(input('Enter number: '))
+num2 = int(input('Enter number: '))
+num3 = int(input('Enter number: '))
+num4 = int(input('Enter number: '))
+num5 = int(input('Enter number: '))
+
+print('Maximum of the numbers: ', max(num1, num2, num3, num4, num5))
