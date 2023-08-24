9bdbe24cea376e3a010a248f6e72f471595f5b44
mkskh <maksym.kharchenko@dci-student.org>
2023-08-24 13:23:31 +0200

    task3 completed with for loop

--- task3.py
+++ task3.py
@@ -1,7 +1,18 @@
-num1 = int(input('Enter number: '))
-num2 = int(input('Enter number: '))
-num3 = int(input('Enter number: '))
-num4 = int(input('Enter number: '))
-num5 = int(input('Enter number: '))
+# num1 = int(input('Enter number: '))
+# num2 = int(input('Enter number: '))
+# num3 = int(input('Enter number: '))
+# num4 = int(input('Enter number: '))
+# num5 = int(input('Enter number: '))
 
-print('Maximum of the numbers: ', max(num1, num2, num3, num4, num5))
+# print('Maximum of the numbers: ', max(num1, num2, num3, num4, num5))
+
+
+
+
+list = []
+
+for i in range(5):
+    tempList = []
+    tempList = int(input('Enter number: '))
+    list.append (tempList)
+print('Maximum of the numbers: ', max(list))
