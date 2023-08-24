372e406485dce4500ccc9701c79607d32fd3c1b5
mkskh <maksym.kharchenko@dci-student.org>
2023-08-24 11:56:20 +0200

    task2 completed with for loop

--- task2.py
+++ task2.py
@@ -1,5 +1,18 @@
-num1 = int(input('Enter number: '))
-num2 = int(input('Enter number: '))
-num3 = int(input('Enter number: '))
+# part 1 (23.08)
 
-print('Sum of the numbers: ', num1 + num2 + num3)
+# num1 = int(input('Enter number: '))
+# num2 = int(input('Enter number: '))
+# num3 = int(input('Enter number: '))
+
+# print('Sum of the numbers: ', num1 + num2 + num3)
+
+
+
+
+# part 2 (24.08)
+
+sum = 0
+
+for i in range(3):
+    sum += int(input('Enter number: ')) 
+print('Sum of the numbers: ', sum)
